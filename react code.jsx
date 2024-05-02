3259971249bd78ef2f886eaffe59095358fb210a
Yogaprakash415 <162480811+Yogaprakash415@users.noreply.github.com>
2024-05-02 18:57:33 +0530

    Update react code.jsx

--- react code.jsx
+++ react code.jsx
@@ -31,3 +31,34 @@ export default ChildComponent;
         return <button>Childcount{value}</button>
     }
 export default ChildComponent;
+
+
+-----------------------------------------------------------------------------------------
+   // props another method
+    function ChildComponent({ value }) {
+    return (
+        < form >
+            <fieldset>
+                <legend>{value.name}</legend>
+                <p>{value.desc}</p>
+            </fieldset>
+        </form >
+    );
+}
+function ParentComponent() {
+    let movies = [
+        { name: "gilli", desc: "vijay and tirisha" },
+        { name: 'ayan', desc: 'surya and tamana' },
+        { name: 'I', desc: 'vikram and Amy Jackson' },
+    ];
+    return (<>
+        {
+            movies.map((value, index) => {
+                return <ChildComponent key={index} value={value} />
+            })
+        }
+        <br />
+    </>)
+}
+
+export default ParentComponent;
